6011188873c4741f6eeaa69274b171422756fbfa
Godfrey Davis <gdavis42@cnm.edu>
2017-07-24 16:56:44 -0600

    creating php filed...

--- php/classes/profile.php
+++ php/classes/profile.php
@@ -7,4 +7,17 @@ require_once("autoload.php");
  **/
 class  Profile implements \JsonSerializable{
 	use ValidateDate;
-}
+/**
+ * primary key for Profile It has to be private
+ */
+	private $profileId;
+/**
+ * profile hash and salt has to be set to private
+ */
+	private $profileHash;
+	private $profilSalt;
+	/**
+	 * @param int\null $profileId Id of profile
+	 * @param varchar\(128)  $profileHash the password of user
+	 * @param var $profileSalt
+	 */
