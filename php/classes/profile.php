e45f26513a0dc9ae446080cd1a3a879c52da4fed
Godfrey Davis <gdavis42@cnm.edu>
2017-07-25 15:51:46 -0600

    creating php filed...

--- php/classes/profile.php
+++ php/classes/profile.php
@@ -1,5 +1,7 @@
 <?php
-
+/**
+ * Creates variables for table Profile to be updated  from end user input to database
+ */
 	class Profile implements \JsonSerializable {
 		use ValidateDate;
 		private $profileId;
@@ -7,23 +9,37 @@
 		private $profileSalt;
 		private $profileUsername;
 
-		public function getProfileId() {
-			return ($this->profileId);
-		}
-
-		public function _construct(?int $newProfileId, string $newProfileHash, string $newProfileSalt, string $newProfileUsername) {
+		public function __construct(?int $newProfileId, string $newProfileHash, string $newProfileSalt, string $newProfileUsername) {
 			try {
 				$this->setProfileId($newProfileId);
 				$this->setProfileHash($newProfileHash);
 				$this->setProfileSalt($newProfileSalt);
-				$this->SetProfileUserName($newProfileUsername);
+				$this->setProfileUserName($newProfileUsername);
 			} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
 				$exceptionType = get_class($exception);
 				throw(new $exceptionType($exceptionType($exception->getMessage(), 0, $exception)));
-
 			}
+		}
+
+		public function getProfileId() {
+			return ($this->profileId);
+		}
+
+		public function setProfileId(?int $newProfileId): void {
+			if($newProfileId === null) {
+				$this->profileId = null;
+				return;
+			}
+			if($newProfileId <= 0) {
+				throw(new \RangeException("this profile is not positive"));
+			}
+			$this->profileId = $newProfileId;
+		}
 
 
 		}
-	}
-	public function
+
+
+
+
+
