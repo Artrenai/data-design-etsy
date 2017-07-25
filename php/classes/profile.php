4f297189a0f073c0869969db76c3c0626bb63352
Godfrey Davis <gdavis42@cnm.edu>
2017-07-25 12:05:51 -0600

    creating php filed...

--- php/classes/profile.php
+++ php/classes/profile.php
@@ -1,20 +1,29 @@
 <?php
 
 	class Profile implements \JsonSerializable {
-					use ValidateDate;
-				private $profileId;
-				private $profileHash;
-				private $profileSalt;
-				private $profileUsername;
-				public function getProfileId() {
-				return ($this->profileId);
+		use ValidateDate;
+		private $profileId;
+		private $profileHash;
+		private $profileSalt;
+		private $profileUsername;
+
+		public function getProfileId() {
+			return ($this->profileId);
 		}
 
-	public function _construct(?int $newProfileId, string $newProfileHash, string $newProfileSalt, string $newProfileUsername ){
-			try{
+		public function _construct(?int $newProfileId, string $newProfileHash, string $newProfileSalt, string $newProfileUsername) {
+			try {
 				$this->setProfileId($newProfileId);
 				$this->setProfileHash($newProfileHash);
 				$this->setProfileSalt($newProfileSalt);
 				$this->SetProfileUserName($newProfileUsername);
+			} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
+				$exceptionType = get_class($exception);
+				throw(new $exceptionType($exceptionType($exception->getMessage(), 0, $exception)));
+
 			}
-			catch(\InvalidArgumentException|)
+
+
+		}
+	}
+	public function
